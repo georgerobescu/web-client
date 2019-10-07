0a8846321b2a06848d717046b9791d6bf86c99ce
pavelzubov <pasha224@yandex.ru>
2019-10-07 17:10:49 +0300

    Change redux HOC to hook

--- packages/shared/components/facet-cards/faset-cards-container.tsx
+++ packages/shared/components/facet-cards/faset-cards-container.tsx
@@ -1,6 +1,6 @@
 import { FundFacet, PlatformInfo, ProgramFacet } from "gv-api-web";
 import * as React from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { platformDataSelector } from "shared/reducers/platform-reducer";
 import { RootState } from "shared/reducers/root-reducer";
@@ -9,44 +9,36 @@ import { composeFacetUrlFunc } from "./facet-card";
 import FacetCards from "./facet-cards";
 import FacetCardsStub from "./facet-cards-stub";
 
-export const _FacetCardsContainer: React.FC<Props> = ({
-  facets,
-  title,
-  composeFacetUrl
-}) => (
-  <FacetCards
-    condition={!!facets.length}
-    loader={<FacetCardsStub />}
-    title={title}
-    facets={facets}
-    composeFacetUrl={composeFacetUrl}
-  />
-);
+export const _FacetCardsContainer: React.FC<Props> = props => {
+  const { title, composeFacetUrl } = props;
+  const facets = useSelector((state: RootState) =>
+    facetsSelector(state, props)
+  );
+  return (
+    <FacetCards
+      condition={!!facets.length}
+      loader={<FacetCardsStub />}
+      title={title}
+      facets={facets}
+      composeFacetUrl={composeFacetUrl}
+    />
+  );
+};
 
 const facetsSelector = createSelector<
   RootState,
-  OwnProps,
+  Props,
   PlatformInfo | undefined,
   ASSETS_FACETS,
   Array<ProgramFacet & FundFacet>
 >(
   (state: RootState) => platformDataSelector(state),
-  (state: RootState, props: OwnProps) => props.assetsFacets,
+  (state: RootState, props: Props) => props.assetsFacets,
   (data: PlatformInfo | undefined, assetsFacets: ASSETS_FACETS) =>
     (data ? data[assetsFacets] : []) as Array<ProgramFacet & FundFacet>
 );
 
-const mapStateToProps = (state: RootState, props: OwnProps): StateProps => ({
-  facets: facetsSelector(state, props)
-});
-
-interface StateProps {
-  facets: Array<ProgramFacet & FundFacet>;
-}
-
-interface Props extends OwnProps, StateProps {}
-
-interface OwnProps {
+interface Props {
   title: string;
   composeFacetUrl: composeFacetUrlFunc;
   assetsFacets: ASSETS_FACETS;
@@ -57,7 +49,5 @@ export enum ASSETS_FACETS {
   PROGRAMS = "programsFacets"
 }
 
-const FacetCardsContainer = connect<StateProps, null, OwnProps, RootState>(
-  mapStateToProps
-)(_FacetCardsContainer);
+const FacetCardsContainer = React.memo(_FacetCardsContainer);
 export default FacetCardsContainer;
