ef4e765fd0c4a9fdfebda43ab9fe53998349dc4f
pavelzubov <pasha224@yandex.ru>
2019-10-08 14:26:42 +0300

    Add fix trailing slash

--- packages/shared/server/server.js
+++ packages/shared/server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const nextI18NextMiddleware = require("next-i18next/middleware");
 const nextI18next = require("shared/i18n");
+const { parse } = require("url");
 
 module.exports = async app => {
   const handle = app.getRequestHandler();
@@ -9,7 +10,15 @@ module.exports = async app => {
   const server = express();
   const port = process.env.PORT || 3000;
   server.use(nextI18NextMiddleware(nextI18next));
-  server.get("*", (req, res) => handle(req, res));
+  server.get("*", (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+    if (pathname.length > 1 && pathname.slice(-1) === "/") {
+      app.render(req, res, pathname.slice(0, -1), query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  });
 
   server.listen(port, err => {
     if (err) throw err;
